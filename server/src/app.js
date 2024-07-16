8858340d6b61e42141fb4551db9ba09e6146e5fe
Nattapon Kumyaito <nattaponk@gmail.com>
2024-07-16 08:45:30 +0700

    update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,20 @@
-console.log('hello nodejs world');
+let express = require('express');
+const app = express();
+
+app.get('/status', function(req, res){
+    console.log('------------------------------');
+    console.log(req);
+    console.log('------------------------------');
+    res.send('Hello nodejs server');
+})
+
+app.get('/hello/:person', function (req,res) {
+    console.log('hello - ' + req.params.person)
+    res.send('sey hello with ' + req.params.person)
+})
+
+let port = 8081
+
+app.listen(port, function(){
+    console.log('Server running on http://localhost:' + port)
+});
