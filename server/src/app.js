4d0d913808592ee2d5181f0c95caa697333d54c9
Nattapon Kumyaito <nattaponk@gmail.com>
2024-07-16 09:46:46 +0700

    add post user api

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,10 @@
 let express = require('express');
+let bodyParser = require('body-parser');
+
+
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/status', function(req, res){
     console.log('------------------------------');
@@ -9,9 +14,25 @@ app.get('/status', function(req, res){
 })
 
 app.get('/hello/:person', function (req,res) {
-    console.log('hello - ' + req.params.person)
-    res.send('sey hello with ' + req.params.person)
+    console.log('hello - ' + req.params.person);
+    res.send('sey hello with ' + req.params.person);
 })
+
+app.get('/user/:userId', function(req, res){
+    res.send('ดูข้อมูล user คนที่ ' + req.params.userId);
+})
+
+app.get('/users', function(req, res){
+    res.send('ดูข้อมูล user ทุกคน');
+})
+
+app.post('/user/', function(req, res){
+    res.send("สร้าง user "+ JSON.stringify(req.body.username));
+})
+
+
+
+
 
 let port = 8081
 
