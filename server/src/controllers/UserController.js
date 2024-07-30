8a76f95e33cdfdfd09d013868968fef29714a979
Nattapon Kumyaito <nattaponk@gmail.com>
2024-07-30 09:57:08 +0700

    Edit UserController.js

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -1,9 +1,25 @@
+const {User} = require('../models');
+
 module.exports = {
-    index(req, res){
-        res.send('ดูข้อมูล user ทุกคน');
+    async index(req, res){
+        try{
+            const users = await User.findAll();
+            res.send(users);
+        } catch(err){
+            res.status(500).send({
+                error: 'เกิดข้อผิดพลาดในการดึงข้อมูล user'
+            })
+        }
     },
-    create(req, res){
-        res.send("สร้าง user "+ JSON.stringify(req.body.username));
+    async create(req, res){
+        try {
+            const user = await User.create(req.body);
+            res.send(user.toJSON());
+        } catch (err) {
+            res.status(400).send({
+                error: 'มีข้อผิดพลาดในการสร้าง user'
+            })
+        }
     },
     put(req, res){
         res.send('แก้ไข user คนที่ '+ req.params.userId + ' ข้อมูลที่แก้ไข' + JSON.stringify(req.body));
