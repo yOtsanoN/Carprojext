1daf765bc0e2c25dacc22f3bc0fd46e0f2dc523f
Nattapon Kumyaito <nattaponk@gmail.com>
2024-08-09 10:01:41 +0700

    add cors

--- client/src/components/User/Index.vue
+++ client/src/components/User/Index.vue
@@ -6,7 +6,12 @@
 
 <script>
 export default {
-
+    created(){
+        this.$http.get('http://localhost:8081/users')
+        .then(function (response) {
+            console.log(response);
+        })
+    }
 }
 </script>
 
