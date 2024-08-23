499862f77f75af6e82acb093b52bb42b4c9d54b6
Nattapon Kumyaito <nattaponk@gmail.com>
2024-08-23 09:50:32 +0700

    use axios 1.5.0

--- client/src/components/User/Index.vue
+++ client/src/components/User/Index.vue
@@ -1,20 +1,17 @@
 <template>
-    <div>
-        <h1>Get All User</h1>
-    </div>
+  <div>
+    <h1>Get All User</h1>
+  </div>
 </template>
 
 <script>
+import UsersService from "@/services/UsersService";
 export default {
-    created(){
-        this.$http.get('http://localhost:8081/users')
-        .then(function (response) {
-            console.log(response);
-        })
-    }
-}
+  async created() {
+    let results = (await UsersService.index()).data;
+    console.log(results);
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
