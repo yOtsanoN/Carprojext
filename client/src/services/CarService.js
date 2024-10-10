be791e63d07f115a8a2e228b090dcf81b43b0746
dfvfdvdff <sdfdfdfds@gmail.com>
2024-10-10 22:23:42 +0700

    add CarService Api

---
+++ client/src/services/CarService.js
@@ -0,0 +1,19 @@
+import Api from '@/services/Api'
+
+export default {
+    index() {
+        return Api().get('cars'); // Get all cars
+    },
+    show(carId) {
+        return Api().get('car/' + carId); // Get a car by ID
+    },
+    post(car) {
+        return Api().post('car', car); // Create a new car
+    },
+    put(car) {
+        return Api().put('car/' + car.id, car); // Update a car
+    },
+    delete(car) {
+        return Api().delete('car/' + car.id); // Delete a car
+    },
+}
